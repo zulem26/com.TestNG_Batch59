391462cf3a0ab3d3667a7a6a2a2227363beb62c3
zulem26 <pruvanetastore@gmail.com>
2022-05-30 05:48:19 +0300

    ikinci

--- src/test/java/tests/day16_notations/C07_SoftAssert.java
+++ src/test/java/tests/day16_notations/C07_SoftAssert.java
@@ -7,7 +7,11 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import utilities.TestBase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class C07_SoftAssert extends TestBase {
 
@@ -51,14 +55,21 @@ public class C07_SoftAssert extends TestBase {
 
         // 10. soft assert kullanarak DropDown listesinin su secenekleri oldugunu test edin
         List<WebElement> optionList = select.getOptions();
-        for (WebElement each:optionList){
+        //Webelementlerden olusan listi lambda expression kullanarak string'e donusturup, sort ettik
+        List<String> optionsListString = optionList.stream().map(WebElement::getText).sorted().collect(Collectors.toList());
+        List<String> expectedList = new ArrayList<>(Arrays.asList("Select One",
+                "Australia (dollar)", "Canada (dollar)", "Switzerland (franc)", "China (yuan)",
+                "Denmark (krone)", "Eurozone (euro)", "Great Britain (pound)", "Hong Kong (dollar)", "Japan (yen)", "Mexico (peso)", "Norway (krone)", "" +
+                        "New Zealand (dollar)", "Sweden (krona)", "Singapore (dollar)", "Thailand (baht)"));
+        Collections.sort(expectedList);//expectedlisteyi sort ettik
+        for (int i = 0; i < optionList.size(); i++) {
+            softAssert.assertEquals(optionsListString.get(i), expectedList.get(i), "DropDownListesi eslesmiyor");
         }
-
-
+        softAssert.assertAll();
         // "Select One", "Australia (dollar)", "Canada (dollar)","Switzerland (franc)","China (yuan)",
-        // "Denmark (krone)","Eurozone (euro)","Great Britain (pound)","Hong Kong (dollar)",
-        // "Japan (yen)","Mexico (peso)","Norway (krone)","New Zealand (dollar)","Sweden (krona)",
-        // "Singapore (dollar)","Thailand (baht)"
+        // "Denmark (krone)","Eurozone (euro)","Great Britain (pound)","Hong Kong (dollar)","Japan (yen)",
+        // "Mexico (peso)","Norway (krone)","New Zealand (dollar)","Sweden (krona)","Singapore (dollar)","Thailand (baht)"
+
 
 
     }
